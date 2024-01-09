8112cf3ecc0e98a39c7602641a9e1012057f1fc2
Abdulvahobjon <abdulvahob0714@gmail.com>
2024-01-09 22:56:26 +0500

    commid

--- zip-file/js/app.js
+++ zip-file/js/app.js
@@ -3,11 +3,11 @@ let btn  = document.querySelector('#btn')
 let input = document.querySelector('input')
 
 btn.addEventListener('click' , function(){
-    const files  = input.files
     let zip = new JSZip()
-    files.forEach(element => {
-        zip.file(element.name , element)
-    });
+    for (var i = 0; i < input.files.length; i++) {
+        var file = input.files[i];
+        zip.file(file.name, file);
+    }
     zip.generateAsync({type:"blob"})
     .then(function(info){
         saveAs(info , "info.zip")
