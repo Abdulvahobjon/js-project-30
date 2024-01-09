1637e85660066810688612bf6758363536d31568
Abdulvahobjon <abdulvahob0714@gmail.com>
2024-01-09 23:04:51 +0500

    commid

--- zip-file/js/app.js
+++ zip-file/js/app.js
@@ -1,6 +1,16 @@
 
 let btn  = document.querySelector('#btn')
 let input = document.querySelector('input')
+let span  = document.querySelector('span')
+let label  = document.querySelector('span')
+
+input.addEventListener('change' , function(){
+    if(input.files.length > 0){
+        span.textContent = input.files.length  + " " + "ta file bor"
+    } else{
+        span.textContent = "file yoq"
+    }
+})
 
 btn.addEventListener('click' , function(){
     let zip = new JSZip()
