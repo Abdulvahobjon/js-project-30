9fb9ab5cfb0319a90bad219a0819176d99470949
Abdulvahobjon <abdulvahob0714@gmail.com>
2024-01-18 12:51:34 +0500

    commid

---
+++ gsap/js/app.js
@@ -0,0 +1,14 @@
+
+gsap.to(".img" , {
+  scrollTrigger:{
+    trigger: "#page1",
+    start: 'top top',
+    end:"bottom top",
+    // markers:true,
+    pin:"#page1",
+    scrub:4,
+    endTrigger:".last"
+  },
+  y:-3500
+})
+
